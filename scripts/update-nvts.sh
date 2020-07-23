a694e95c7a14c7871e2301c65ae2301816efe67a
disarmm <49657913+disarmm@users.noreply.github.com>
2020-07-23 01:35:54 -0500

    Update update-nvts.sh

--- scripts/update-nvts.sh
+++ scripts/update-nvts.sh
@@ -4,5 +4,5 @@ set -Eeuo pipefail
 while true; do
 	echo "Updating NVTs..."
 	su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
-	sleep 43200
+	sleep 5
 done
