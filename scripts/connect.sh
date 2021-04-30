d0fdbdd9f4544091d8ef13b437a5e12345aff276
Joshua Schmitt <joshua@pixelcubed.com>
2021-04-30 15:41:12 -0500

    fix autossh log

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -2,7 +2,9 @@
 set -Eeuo pipefail
 
 MASTER_PORT=${MASTER_PORT:-22}
+export AUTOSSH_LOGLEVEL=7
+export AUTOSSH_LOGFILE=/usr/local/var/log/gvm/ssh-connection.log
 
 SCANNER_ID=$(cat /data/scannerid)
 
-autossh -N -T -i /data/ssh/key -o ExitOnForwardFailure=yes -o UserKnownHostsFile=/data/ssh/known_hosts -p $MASTER_PORT -R /sockets/$SCANNER_ID.sock:/data/ospd.sock gvm@$MASTER_ADDRESS
+autossh -M 0 -N -T -i /data/ssh/key -o ExitOnForwardFailure=yes -o UserKnownHostsFile=/data/ssh/known_hosts -p $MASTER_PORT -R /sockets/$SCANNER_ID.sock:/data/ospd.sock gvm@$MASTER_ADDRESS
