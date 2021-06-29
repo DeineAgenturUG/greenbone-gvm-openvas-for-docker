5e464050a9de17800d08455b0736ae4995cc8156
Josef Fröhle <froehle@b1-systems.de>
2021-06-29 21:53:25 +0200

    Move to Alpine Image

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,127 +3,126 @@ set -Eeuo pipefail
 
 MASTER_PORT=${MASTER_PORT:-22}
 
-if [ -z $MASTER_ADDRESS ]; then
+if [ -z "${MASTER_ADDRESS}" ]; then
 	echo "ERROR: The environment variable \"MASTER_ADDRESS\" is not set"
 	exit 1
 fi
 
-if  [ ! -d /data ]; then
-	echo "Creating Data folder..."
-        mkdir /data
-fi
-
-if [ ! -f "/firstrun" ]; then
-	echo "Running first start configuration..."
-
-	echo "Creating Openvas NVT sync user..."
-	useradd --home-dir /usr/local/share/openvas openvas-sync
-	chown openvas-sync:openvas-sync -R /usr/local/share/openvas
-	chown openvas-sync:openvas-sync -R /usr/local/var/lib/openvas
-
-	touch /firstrun
-fi
-
-if [ ! -f "/data/scannerid" ]; then
+if [ ! -f "/var/lib/gvm/.scannerid" ]; then
 	echo "Generating scanner id..."
-	
-	echo $(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1) > /data/scannerid
+
+	cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1 >/var/lib/gvm/.scannerid
 fi
 
-if  [ ! -d /data/ssh ]; then
-	mkdir /data/ssh
+if [ ! -d /var/lib/gvm/.ssh ]; then
+	mkdir /var/lib/gvm/.ssh
 fi
 
-if  [ ! -f /data/ssh/known_hosts ]; then
+if [ ! -f /var/lib/gvm/.ssh/known_hosts ]; then
 	echo "Getting Master SSH key..."
-	ssh-keyscan -t ed25519 -p $MASTER_PORT $MASTER_ADDRESS > /data/ssh/known_hosts.temp
-	mv /data/ssh/known_hosts.temp /data/ssh/known_hosts
+	ssh-keyscan -t ed25519 -p "${MASTER_PORT}" "${MASTER_ADDRESS}" >/var/lib/gvm/.ssh/known_hosts.temp
+	mv /var/lib/gvm/.ssh/known_hosts.temp /var/lib/gvm/.ssh/known_hosts
 fi
 
-if  [ ! -f /data/ssh/key ]; then
+if [ ! -f /var/lib/gvm/.ssh/key ]; then
 	echo "Setup SSH key..."
-	ssh-keygen -t ed25519 -f /data/ssh/key -N "" -C "$(cat /data/scannerid)"
+	ssh-keygen -t ed25519 -f /var/lib/gvm/.ssh/key -N "" -C "$(cat /var/lib/gvm/.scannerid)"
 fi
+
+## Start Redis
 
 if [ ! -d "/run/redis" ]; then
 	mkdir /run/redis
 fi
-if  [ -S /run/redis/redis.sock ]; then
-        rm /run/redis/redis.sock
+
+if [ -S /run/redis/redis.sock ]; then
+	rm /run/redis/redis.sock
 fi
-redis-server --unixsocket /run/redis/redis.sock --unixsocketperm 700 --timeout 0 --databases 128 --maxclients 4096 --daemonize yes --port 6379 --bind 0.0.0.0
+
+if [ ! -d "/run/redis-openvas" ]; then
+	echo "create /run/redis-openvas"
+	mkdir /run/redis-openvas
+fi
+
+if [ -S /run/redis-openvas/redis.sock ]; then
+	rm /run/redis-openvas/redis.sock
+fi
+
+${SUPVISD} start redis
+${SUPVISD} status redis
 
 echo "Wait for redis socket to be created..."
-while  [ ! -S /run/redis/redis.sock ]; do
-        sleep 1
-done
-
-echo "Testing redis status..."
-X="$(redis-cli -s /run/redis/redis.sock ping)"
-while  [ "${X}" != "PONG" ]; do
-        echo "Redis not yet ready..."
-        sleep 1
-        X="$(redis-cli -s /run/redis/redis.sock ping)"
-done
-echo "Redis ready."
-
-if  [ ! -d /data/plugins ]; then
-	echo "Creating NVT Plugins folder..."
-	mkdir /data/plugins
-fi
-
-if [ ! -h /usr/local/var/lib/openvas/plugins ]; then
-	echo "Fixing NVT Plugins folder..."
-	rm -rf /usr/local/var/lib/openvas/plugins
-	ln -s /data/plugins /usr/local/var/lib/openvas/plugins
-	chown openvas-sync:openvas-sync -R /data/plugins
-	chown openvas-sync:openvas-sync -R /usr/local/var/lib/openvas/plugins
-fi
-
-echo "Updating NVTs..."
-su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
-echo "+++++++++++++++++++++++++++++++++++"
-echo "+ Enabling Automating NVT updates +"
-echo "+++++++++++++++++++++++++++++++++++"
-/update-nvts.sh & disown
-sleep 5
-
-if [ -f /var/run/ospd.pid ]; then
-  rm /var/run/ospd.pid
-fi
-
-if [ -S /data/ospd.sock ]; then
-  rm /data/ospd.sock
-fi
-
-if [ ! -d /var/run/ospd ]; then
-  mkdir /var/run/ospd
-fi
-
-echo "Starting Open Scanner Protocol daemon for OpenVAS..."
-ospd-openvas --log-file /usr/local/var/log/gvm/ospd-openvas.log --unix-socket /data/ospd.sock --log-level INFO
-
-while  [ ! -S /data/ospd.sock ]; do
+while [ ! -S /run/redis-openvas/redis.sock ]; do
 	sleep 1
 done
 
-chmod 666 /data/ospd.sock
+echo "Testing redis status..."
+X="$(redis-cli -s /run/redis-openvas/redis.sock ping)"
+while [ "${X}" != "PONG" ]; do
+	echo "Redis not yet ready..."
+	sleep 1
+	X="$(redis-cli -s /run/redis-openvas/redis.sock ping)"
+done
+echo "Redis ready."
 
-touch /usr/local/var/log/gvm/ssh-connection.log
-/connect.sh &
+echo "+++++++++++++++++++++++++++++++++++"
+echo "+ Enabling Automating NVT updates +"
+echo "+++++++++++++++++++++++++++++++++++"
+${SUPVISD} start GVMUpdate
+if [ "${DEBUG}" == "Y" ]; then
+	${SUPVISD} status GVMUpdate
+fi
+sleep 5
+
+#############################
+# Remove leftover pid files #
+#############################
+
+if [ -f /var/run/ospd.pid ]; then
+	rm /var/run/ospd.pid
+fi
+
+if [ -S /tmp/ospd.sock ]; then
+	rm /tmp/ospd.sock
+fi
+
+if [ -S /var/run/ospd/ospd.sock ]; then
+	rm /var/run/ospd/ospd.sock
+fi
+
+if [ ! -d /var/run/ospd ]; then
+	mkdir /var/run/ospd
+fi
+
+echo "Starting Open Scanner Protocol daemon for OpenVAS..."
+${SUPVISD} start ospd-openvas
+if [ "${DEBUG}" == "Y" ]; then
+	${SUPVISD} status ospd-openvas
+fi
+
+while [ ! -S /var/run/ospd/ospd.sock ]; do
+	sleep 1
+done
+
+SCANNER_ID=$(cat /var/lib/gvm/.scannerid)
+export SCANNER_ID=${SCANNER_ID}
+${SUPVISD} start autossh
+if [ "${DEBUG}" == "Y" ]; then
+	${SUPVISD} status autossh
+fi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+ Your OpenVAS Scanner container is now ready to use! +"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo ""
 echo "-------------------------------------------------------"
-echo "Scanner id: $(cat /data/scannerid)"
-echo "Public key: $(cat /data/ssh/key.pub)"
+echo "Scanner id: $(cat /var/lib/gvm/.scannerid)"
+echo "Public key: $(cat /var/lib/gvm/.ssh/key.pub)"
 echo "Master host key (Check that it matches the public key from the master):"
-cat /data/ssh/known_hosts
+cat /var/lib/gvm/.ssh/known_hosts
 echo "-------------------------------------------------------"
 echo ""
 echo "++++++++++++++++"
 echo "+ Tailing logs +"
 echo "++++++++++++++++"
-tail -F /usr/local/var/log/gvm/*
+tail -F /var/log/gvm/*
--- scripts/update-nvts.sh
+++ scripts/update-nvts.sh
@@ -2,8 +2,27 @@
 # This script update the NVTs in the background every 12 hours.
 set -Eeuo pipefail
 
+if [ ! -f "/var/lib/gvm/.firstsync" ]; then
+	echo "Downloading data TAR to speed up first sync..."
+	mkdir /tmp/data
+
+	echo "Extracting internal data TAR..."
+	tar --extract --file=/opt/gvm-sync-data.tar.xz --directory=/tmp/data
+
+	chown gvm:gvm -R /tmp/data
+
+	cp -a /tmp/data/nvt-feed/* /var/lib/openvas/plugins
+	chown gvm:gvm -R /var/lib/openvas
+
+	find /var/lib/openvas/ -type d -exec chmod 755 {} +
+	find /var/lib/openvas/ -type f -exec chmod 644 {} +
+
+	rm /tmp/data.tar.xz
+	rm -r /tmp/data
+fi
+
 while true; do
 	echo "Running Automatic NVT update..."
-	su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
+	su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /var/lib/openvas/plugins" gvm
 	sleep 43200
 done
