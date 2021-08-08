5e55f85c1ae4ef06b66f236405d75f7fb0628e16
Josef Fröhle <froehle@b1-systems.de>
2021-08-08 15:58:22 +0200

    Fix Build

--- builder/build_gvm.sh
+++ builder/build_gvm.sh
@@ -16,14 +16,26 @@ BuilldAndSingMe() {
     sleep 1
     abuild -c -r -P /target
     sleep 1
-    cd /target/community/x86_64/ || exit
+    # ignore all python packages
+    if [ "$1" == "gvm-tools" ] || [ "$1" == "ospd" ] || [ "$1" == "ospd-openvas" ] || [ "$1" == "python-gvm" ]; then
+        cd /target/community/noarch/ || exit
+        cp ../x86_64/py3-gvm*.apk ./ || true
+        cp ../x86_64/gvm-tools*.apk ./ || true
+        cp ../x86_64/ospd*.apk ./ || true
+        sleep 1
+        apk index -o APKINDEX.tar.gz *.apk
+        abuild-sign APKINDEX.tar.gz
+    else
+        cd /target/community/x86_64/ || exit
+    fi
+
     sleep 1
     apk index -o APKINDEX.tar.gz *.apk
     abuild-sign APKINDEX.tar.gz
 }
 BuildThis() {
     cd /work/community/"$1"/ || exit
-    BuilldAndSingMe
+    BuilldAndSingMe "$1"
 }
 
 BuildThis gvm-libs
