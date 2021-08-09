bae1fda991f8857adc6fc625c9990b9c71a66d52
Josef Fröhle <froehle@b1-systems.de>
2021-08-09 17:56:50 +0200

    Fix OpenVAS Container

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,25 +5,41 @@ export SUPVISD=${SUPVISD:-supervisorctl}
 export TZ=${TZ:-UTC}
 export MASTER_PORT=${MASTER_PORT:-22}
 export MASTER_ADDRESS=${MASTER_ADDRESS}
-export AUTOSSH_LOGLEVEL=7
+export AUTOSSH_DEBUG=${AUTOSSH_DEBUG:-0}
+export AUTOSSH_LOGLEVEL=${AUTOSSH_DEBUG:-7}
 export AUTOSSH_LOGFILE=/var/log/gvm/ssh-connection.log
+export SCANNER_ID
 
 if [ -z "${MASTER_ADDRESS}" ]; then
     echo "ERROR: The environment variable \"MASTER_ADDRESS\" is not set"
     exit 1
 fi
 
-if [ "$1" == "/usr/bin/supervisord" ]; then
-    #  exec /start.sh
-    echo "GVM Started but with > supervisor <"
-    if [ ! -f "/firstrun" ]; then
-        echo "Running first start configuration..."
+if [ ! -d /var/lib/gvm/.ssh ]; then
+    mkdir -p /var/lib/gvm/.ssh
+fi
 
-        ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" >/etc/timezone
+if [ ! -f "/var/lib/gvm/.scannerid" ]; then
+    echo "Generating scanner id..."
+    set +e
+    cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1 >/var/lib/gvm/.scannerid
+    set -e
+fi
 
-        touch /firstrun
-        touch /var/log/gvm/ssh-connection.log
-    fi
+SCANNER_ID=$(cat /var/lib/gvm/.scannerid)
+
+echo "GVM Started but with > supervisor <"
+if [ ! -f "/firstrun" ]; then
+    echo "Running first start configuration..."
+
+    ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" >/etc/timezone
+
+    touch /firstrun
+    touch /var/log/gvm/ssh-connection.log
+fi
+if [ -f "/var/lib/gvm/.firststart" ]; then
+    rm /var/lib/gvm/.firststart
+    touch /var/lib/gvm/.secondstart
 fi
 
 exec "$@"
