f8ff61554a0358780cc1db7c6536c2f4a5b2e9d5
Octavian Grigorescu <octavian.grigorescu.octa@gmail.com>
2022-02-21 14:05:05 +0200

    Update build_gvm.sh

--- builder/build_gvm.sh
+++ builder/build_gvm.sh
@@ -9,6 +9,9 @@ echo 'I AM:'
 whoami
 cat /etc/apk/keys/build.rsa.pub
 
+echo 'Running env'
+env
+
 mkdir -p /target/community/noarch/
 mkdir -p /target/community/x86_64/
 
@@ -16,13 +19,16 @@ BUILD_CHECKSUM=${CHECKSUM:-0}
 
 BuilldAndSingMe() {
     if [ "${BUILD_CHECKSUM}" == "1" ]; then
+        echo '----------------------- Before abuild checksum'
         abuild checksum
     fi
     sleep 1
+    echo '----------------------- Before abuild'
     abuild -c -r -P /target
     sleep 1
     # ignore all python packages
     if [ "$1" == "nmap" ] || [ "$1" == "gvm-tools" ] || [ "$1" == "ospd" ] || [ "$1" == "ospd-openvas" ] || [ "$1" == "python-gvm" ]; then
+        echo '---------------- I am in IF statement'
         cd /target/community/noarch/ || exit
         cp ../x86_64/nmap-scripts*.apk ./ || true
         cp ../x86_64/nmap-nselibs*.apk ./ || true
@@ -30,9 +36,9 @@ BuilldAndSingMe() {
         cp ../x86_64/gvm-tools*.apk ./ || true
         cp ../x86_64/ospd*.apk ./ || true
         sleep 1
-        echo 'Before apk index'
+        echo '---------------------- Before apk index'
         apk index -o APKINDEX.tar.gz *.apk
-        echo 'Before abuild-sign'
+        echo '---------------------- Before abuild-sign'
         env
         abuild-sign APKINDEX.tar.gz
     else
@@ -40,6 +46,7 @@ BuilldAndSingMe() {
     fi
 
     sleep 1
+    echo 'Before apk index'
     apk index -o APKINDEX.tar.gz *.apk
     echo 'Before signing...'
     abuild-sign APKINDEX.tar.gz
