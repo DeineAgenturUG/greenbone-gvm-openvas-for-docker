d3eb2c15bd42585c2d4e3387e888104438919315
Josef Fröhle <josef.froehle@greenbone.net>
2022-03-11 19:06:59 +0100

    disable openvas-smb for now, it will not compile currently

--- scripts/debian/main_pkgs.sh
+++ scripts/debian/main_pkgs.sh
@@ -211,39 +211,39 @@ sudo apt-get purge -y \
     yarnpkg
 
 # Install required dependencies for openvas-smb
-
-if [[ "$(dpkg --print-architecture)" == "amd64" ]]; then
-
-  sudo apt-get install -y --no-install-recommends \
-      gcc-mingw-w64 \
-      libgnutls28-dev \
-      libglib2.0-dev \
-      libpopt-dev \
-      libunistring-dev \
-      heimdal-dev \
-      libgssapi3-heimdal \
-      libhdb9-heimdal \
-      perl-base
-
-  curl -sSL https://github.com/greenbone/openvas-smb/archive/refs/tags/v${OPENVAS_SMB_VERSION}.tar.gz -o ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz
-  curl -sSL https://github.com/greenbone/openvas-smb/releases/download/v${OPENVAS_SMB_VERSION}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz.asc -o ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz.asc
-
-  gpg --verify ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz.asc ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz
-
-  tar -C ${SOURCE_DIR} -xvzf ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz
-
-  mkdir -p ${BUILD_DIR}/openvas-smb && cd ${BUILD_DIR}/openvas-smb
-
-  cmake ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION} \
-      -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-      -DCMAKE_BUILD_TYPE=Release
-
-  make -j$(nproc)
-  make DESTDIR=${INSTALL_DIR} install
-  sudo cp -rv ${INSTALL_DIR}/* /
-  #rm -rf ${INSTALL_DIR}/*
-
-fi
+#
+#if [[ "$(dpkg --print-architecture)" == "amd64" ]]; then
+#
+#  sudo apt-get install -y --no-install-recommends \
+#      gcc-mingw-w64 \
+#      libgnutls28-dev \
+#      libglib2.0-dev \
+#      libpopt-dev \
+#      libunistring-dev \
+#      heimdal-dev \
+#      libgssapi3-heimdal \
+#      libhdb9-heimdal \
+#      perl-base
+#
+#  curl -sSL https://github.com/greenbone/openvas-smb/archive/refs/tags/v${OPENVAS_SMB_VERSION}.tar.gz -o ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz
+#  curl -sSL https://github.com/greenbone/openvas-smb/releases/download/v${OPENVAS_SMB_VERSION}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz.asc -o ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz.asc
+#
+#  gpg --verify ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz.asc ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz
+#
+#  tar -C ${SOURCE_DIR} -xvzf ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION}.tar.gz
+#
+#  mkdir -p ${BUILD_DIR}/openvas-smb && cd ${BUILD_DIR}/openvas-smb
+#
+#  cmake ${SOURCE_DIR}/openvas-smb-${OPENVAS_SMB_VERSION} \
+#      -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+#      -DCMAKE_BUILD_TYPE=Release
+#
+#  make -j$(nproc)
+#  make DESTDIR=${INSTALL_DIR} install
+#  sudo cp -rv ${INSTALL_DIR}/* /
+#  #rm -rf ${INSTALL_DIR}/*
+#
+#fi
 
 # Install required dependencies for openvas-scanner
 sudo apt-get install -y --no-install-recommends \
