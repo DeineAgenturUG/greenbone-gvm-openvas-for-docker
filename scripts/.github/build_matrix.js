553773c37f50cb3c2e12086889c7998a4885db31
Josef Fröhle <josef.froehle@greenbone.net>
2022-03-11 09:56:31 +0100

    docker-publish-debian.yml: build matrix

--- scripts/.github/build_matrix.js
+++ scripts/.github/build_matrix.js
@@ -5,20 +5,22 @@ let matrix_prepared = {
   BUILD_SUFFIX: ["", "-full", "-data", "-data-full"],
   BUILD_ARGS: ["SETUP=0\n", "SETUP=0\nOPT_PDF=1\n", "SETUP=1\n", "SETUP=1\nOPT_PDF=1\n"]
 }
-let matrix_output = {}
+let matrix_output = {include:[]}
 let latest_version
 
 
 try {
   latest_version = fs.readFileSync('./.checkupdates/latest_version', 'utf8');
-  console.log(latest_version);
 } catch (e) {
-  console.log('Error:', e.stack);
+  process.exit(1)
 }
 
-matrix_output = JSON.parse(JSON.stringify(matrix_prepared))
 matrix_prepared.BUILD_ARGS.forEach((value, index) => {
-  matrix_output.BUILD_ARGS[index] = latest_version + value
+  matrix_output.include[index] = {
+    BUILD_NAME:matrix_prepared.BUILD_NAME[index],
+    BUILD_SUFFIX:matrix_prepared.BUILD_SUFFIX[index],
+    BUILD_ARGS:latest_version + value,
+  }
 })
 
-console.log(matrix_output)
+console.log(JSON.stringify(matrix_output))
