90463e6148c545b10d45de7045cbd04234fe386f
Josef Fröhle <github@josef-froehle.de>
2022-05-25 12:01:42 +0200

    refactor nullmailer config

--- GVMDocker/scripts/addons/02_nullmailer.sh
+++ GVMDocker/scripts/addons/02_nullmailer.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-if [ -n "${MAIL_RELAY_HOST}" ] && [ -n "${MAIL_RELAY_PORT}" ] && [ -n "${MAIL_ADMIN_ADDRESS}" ]; then
-    echo "$(hostname)" >/etc/mailname
-    echo "${MAIL_DEFAULT_DOMAIN:-localhost}" >/etc/nullmailer/defaultdomain
-    echo "${MAIL_ADMIN_ADDRESS}" >/etc/nullmailer/adminaddr
+echo "$(hostname)" >/etc/mailname
+echo "${MAIL_DEFAULT_DOMAIN:-localhost}" >/etc/nullmailer/defaultdomain
+if [ -n "${MAIL_RELAY_HOST}" ] && [ -n "${MAIL_RELAY_PORT}" ]; then
     echo "${MAIL_RELAY_HOST:-localhost} smtp --port=${MAIL_RELAY_PORT:-25} ${MAIL_RELAY_OPTIONS}" >/etc/nullmailer/remotes
+else 
+    echo "localhost discard" >/etc/nullmailer/remotes
+fi
+if [ -n "${MAIL_ADMIN_ADDRESS}" ]; then
+    echo "${MAIL_ADMIN_ADDRESS}" >/etc/nullmailer/adminaddr
+else
+    echo -n >/etc/nullmailer/adminaddr
+fi
+if [ -n "${MAIL_HELOHOST_FQDN}" ]; then
     echo "${MAIL_HELOHOST_FQDN}" >/etc/nullmailer/helohost
+else
+    echo -n >/etc/nullmailer/helohost
 fi
